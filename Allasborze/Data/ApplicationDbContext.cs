6950c28e5c384e7e9ab8d0c5a87f3f4094298d8d
P.Lóránt <pergerlori@gmail.com>
2023-04-25 14:46:46 +0200

    Fix db.

--- Allasborze/Data/ApplicationDbContext.cs
+++ Allasborze/Data/ApplicationDbContext.cs
@@ -6,8 +6,8 @@ namespace Allasborze.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        DbSet<AllasModel> Allasok { get; set; }
-        DbSet<AllasUser> Felhasznalok { get; set; }
+        public DbSet<AllasModel> Allasok { get; set; }
+        public DbSet<AllasUser> Felhasznalok { get; set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
